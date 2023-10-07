1f33eb6858e20cb7c8e944054894b656852c34f4
lunchquader <luca.tabone93@gmail.com>
2023-10-07 17:43:49 +0200

    added func to compute string infix

---
+++ utils/text.py
@@ -0,0 +1,14 @@
+def get_infix(text: str, prefix: str, suffix: str) -> str | None:
+    """ @return an infix within @param text given a specific prefix and a specific suffix. """
+    if not text:
+        return
+    idx: int = text.find(prefix)
+    if idx == -1:
+        return
+    res: str = text[idx+len(prefix):]
+    if not suffix:
+        return res
+    idx = res.find(suffix)
+    if idx == -1:
+        return
+    return res[:idx]
