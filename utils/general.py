b76c151c9bb5b5b8a0845e0b40d01ee1200f282a
lunchquader <luca.tabone93@gmail.com>
2023-10-07 20:01:12 +0200

    bugfix for default params update function

--- utils/general.py
+++ utils/general.py
@@ -2,11 +2,11 @@ from dataclasses import fields, Field, MISSING
 
 
 def update_params_with_defaults(dataclass_type, params: dict[str, any]) -> dict[str, any]:
-    return params | {
-        _field.name: _get_default_value(_field)
-        for _field in fields(dataclass_type)
-        if _field.name in params and params.get(_field.name, None) is None
-    }
+    defaults = {}
+    for _field in fields(dataclass_type):
+        if _field.name in params and params.get(_field.name, None) is None:
+            defaults[_field.name] = _get_default_value(_field)
+    return params | defaults
 
 
 def _get_default_value(field: Field) -> any:
@@ -14,4 +14,3 @@ def _get_default_value(field: Field) -> any:
         return field.default
     if field.default_factory != MISSING:
         return field.default_factory()
-    raise None
