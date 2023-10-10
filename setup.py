bd10bb5bacd0ecd21309518fc83980561a7c1973
lunchquader <luca.tabone93@gmail.com>
2023-10-10 06:39:58 +0200

    added function to prettify all locally stored html files

---
+++ setup.py
@@ -0,0 +1,21 @@
+from os import walk, getcwd
+from bs4 import BeautifulSoup
+from utils.html import remove_tags
+
+
+def beautify_all_html_files():
+    for res in walk(getcwd()):
+        for file_name in res[2]:
+            if not file_name.endswith(".html"):
+                continue
+            file_path: str = f"{res[0]}/{file_name}"
+            with open(file_path, "r+", encoding="utf-8") as file:
+                soup: BeautifulSoup = BeautifulSoup(file.read(), "html.parser")
+                remove_tags(soup=soup, tag_names=["script", "noscript", "style"])
+                file.seek(0)
+                file.write(soup.prettify())
+                file.truncate()
+
+
+if __name__ == '__main__':
+    beautify_all_html_files()
