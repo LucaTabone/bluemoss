f32689311a8b0e1804cc1cd29ce4696b7d78bea7
lunchquader <luca.tabone93@gmail.com>
2023-10-10 06:40:47 +0200

    added second example to show Recipe extraction on reuters.com/world

---
+++ examples/reuters/reuters_classes.py
@@ -0,0 +1,11 @@
+from typings import Dictable
+from dataclasses import dataclass
+
+
+@dataclass
+class Article(Dictable):
+    url: str
+    date: str
+    title: str
+    topic: str
+    img_url: str
---
+++ examples/reuters/reuters_recipe.py
@@ -0,0 +1,28 @@
+from typings import Extract
+from recipe import Recipe, extract
+from examples.reuters.reuters_classes import Article
+
+
+REUTERS_WORLD_NEWS_ARTICLES_RECIPE: Recipe = Recipe(
+    end_idx=None,
+    path_prefix="//",
+    path="li[contains(@class, 'story-collection')]/div[contains(@class, 'media-story-card')]",
+    target=Article,
+    children=[
+        Recipe(
+            path="a",
+            context="url",
+            extract=Extract.HREF_ENDPOINT,
+            transform=lambda endpoint: f"https://reuters.com{endpoint}"
+        ),
+        Recipe(context="date", path="time"),
+        Recipe(context="title", path="h3/a"),
+        Recipe(context="img_url", path="img", extract="src"),
+        Recipe(context="topic", path="span/a", extract=Extract.TEXT)
+    ]
+)
+
+
+with open("./static/world_news.html", "r") as f:
+    for article in extract(REUTERS_WORLD_NEWS_ARTICLES_RECIPE, f.read()):
+        print(article)
