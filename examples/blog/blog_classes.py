936ad4349c69e768025a955b9ad0c94d7fe24f84
lunchquader <luca.tabone93@gmail.com>
2023-10-11 17:01:09 +0200

    provided a fourth example using seths.blog wesbsite

---
+++ examples/blog/blog_classes.py
@@ -0,0 +1,14 @@
+from typings import Dictable
+from dataclasses import dataclass, field
+
+
+@dataclass
+class BlogPost(Dictable):
+    url: str
+    date: str
+    title: str
+    text: str | None = field(default=None, init=False)
+    _text_lines: list[str]
+
+    def __post_init__(self):
+        self.text = "\n\n".join([line.strip() for line in self._text_lines if line.strip()])
---
+++ examples/blog/blog_recipe.py
@@ -0,0 +1,26 @@
+from recipe import Recipe, Range, extract
+from examples.blog.blog_classes import BlogPost
+
+
+BLOG_POSTS_RECIPE: Recipe = Recipe(
+    path_prefix="/html",
+    path="//div[@class='post']",
+    range=Range(0, None),
+    target=BlogPost,
+    children=[
+        Recipe(context="title", path="a"),
+        Recipe(context="url", path="a[@href]"),
+        Recipe(context="date", path="span[@class='date']"),
+        Recipe(
+            path_prefix="",
+            range=Range(0, None),
+            context="_text_lines",
+            path=".//p[not(@*)] | .//li[not(@*)]"
+        )
+    ]
+)
+
+
+with open("./static/blog.html", "r") as f:
+    for article in extract(BLOG_POSTS_RECIPE, f.read()):
+        print(article)
