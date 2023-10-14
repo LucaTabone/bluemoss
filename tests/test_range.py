105e93f95a245ca40c434b03d9b52733c796f1f3
lunchquader <luca.tabone93@gmail.com>
2023-10-14 19:22:59 +0200

    added empty tests for test_range.py

--- tests/test_range.py
+++ tests/test_range.py
@@ -1,1 +1,26 @@
 import pytest
+from bluemoss import Moss, Range, extract
+
+
+def test_find_first():
+    pass
+
+
+def test_find_last():
+    pass
+
+
+def test_find_range():
+    pass
+
+
+def test_find_range_reversed():
+    pass
+
+
+def test_find_all():
+    pass
+
+
+def test_find_all_reversed():
+    pass
