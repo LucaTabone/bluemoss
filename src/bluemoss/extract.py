7ff1da8b2cbedd6cde892b462c7f11b8caa1bf30
lunchquader <luca.tabone93@gmail.com>
2023-10-17 22:06:44 +0200

    propagated Range class refactoring into extract.py

--- src/bluemoss/extract.py
+++ src/bluemoss/extract.py
@@ -1,5 +1,5 @@
 from lxml import html as lxml_html
-from .classes import Ex, BlueMoss, DictableWithTag, PrettyDict
+from .classes import Ex, BlueMoss, Range, DictableWithTag, PrettyDict
 from .utils import (
     clean_text,
     get_domain,
@@ -18,81 +18,81 @@ def extract(moss: BlueMoss, html: str) -> any:
 
 
 def _extract(moss: BlueMoss, root) -> any:
-    if not (nodes := root.xpath(moss.full_path)):
+    if not (elems := root.xpath(moss.full_path)):
         if moss.extract == Ex.FOUND:
             return False
         return
-    end_index: int = len(nodes) if moss.range.end_idx is None else moss.range.end_idx
     try:
-        nodes = nodes[moss.range.start_idx:end_index]
-        if moss.range.reverse:
-            nodes = nodes[::-1]
+        if isinstance(moss.range, int):
+            elems = [elems[moss.range]]
+        elif isinstance(moss.range, Range):
+            elems = moss.range.filter(elems)
     except IndexError:
         return None
     if not moss.nodes:
         res = [
-            moss.transform(_extract_from_leaf_node(moss, node))
-            for node in nodes
+            moss.transform(_extract_from_leaf_node(moss, elem))
+            for elem in elems
         ]
-        return (res[0] if res else None) if moss.range.find_single else res
+        return (res[0] if res else None) if moss.find_single else res
     if moss.target or moss.target_is_dict or moss.target_is_list:
-        res: list = [_build_target_instance(moss, node) for node in nodes]
+        res: list = [_build_target_instance(moss, elem) for elem in elems]
     else:
         res: list = [
-            _extract(_moss, node)
+            _extract(_moss, elem)
             for _moss in moss.nodes
-            for node in nodes
+            for elem in elems
         ]
-    return moss.transform(res[0] if res else None) if moss.range.find_single else moss.transform(res)
+    return moss.transform(res[0] if res else None) if moss.find_single else moss.transform(res)
 
 
-def _extract_from_leaf_node(moss: BlueMoss, node) -> any:
-    if type(node).__name__ == '_ElementUnicodeResult':
-        return str(node)
+def _extract_from_leaf_node(moss: BlueMoss, elem) -> any:
+    if type(elem).__name__ == '_ElementUnicodeResult':
+        return str(elem)
     if isinstance(moss.extract, str):
-        return node.get(moss.extract)
+        return elem.get(moss.extract)
     if not isinstance(moss.extract, Ex):
         raise ValueError(f"The @param moss.extract must be a string or Extract value.")
     match moss.extract:
         case None | Ex.ETREE:
-            return node
+            return elem
         case Ex.FOUND:
-            return node is not None
+            return elem is not None
         case Ex.TEXT:
-            return node.text.strip()
+            return elem.text.strip()
         case Ex.FULL_TEXT:
-            return clean_text(node.text_content().strip())
+            return clean_text(elem.text_content().strip())
         case Ex.TAG:
-            return etree_to_bs4(node)
+            return etree_to_bs4(elem)
         case Ex.TAG_AS_STRING:
-            return etree_to_bs4(node).prettify()
+            return etree_to_bs4(elem).prettify()
         case Ex.HREF:
-            return node.get("href")
+            return elem.get("href")
         case Ex.HREF_QUERY:
-            return get_url_query(node.get("href"))
+            return get_url_query(elem.get("href"))
         case Ex.HREF_DOMAIN:
-            return get_domain(node.get("href"))
+            return get_domain(elem.get("href"))
         case Ex.HREF_ENDPOINT:
-            return get_endpoint(node.get("href"))
+            return get_endpoint(elem.get("href"))
         case Ex.HREF_BASE_DOMAIN:
-            return get_base_domain(node.get("href"))
+            return get_base_domain(elem.get("href"))
         case Ex.HREF_QUERY_PARAMS:
-            return get_url_query_params(node.get("href"))
+            return get_url_query_params(elem.get("href"))
         case Ex.HREF_ENDPOINT_WITH_QUERY:
-            return get_endpoint_with_query(node.get("href"))
+            return get_endpoint_with_query(elem.get("href"))
 
 
-def _build_target_instance(moss: BlueMoss, node):
+def _build_target_instance(moss: BlueMoss, elem):
     if moss.target_is_dict:
-        return PrettyDict({c.key: _extract(c, node) for c in moss.nodes})
+        return PrettyDict({c.key: _extract(c, elem) for c in moss.nodes})
     elif moss.target_is_list:
-        return [_extract(c, node) for c in moss.nodes]
+        return [_extract(c, elem) for c in moss.nodes]
     values: dict[str, any] = {}
     params_with_defaults: set[str] = get_params_with_default_value(moss.target)
     for _moss in moss.nodes:
-        val: any = _extract(_moss, node)
+        val: any = _extract(_moss, elem)
         if not (val is None and _moss.key in params_with_defaults):
             values[_moss.key] = val
     if issubclass(moss.target, DictableWithTag):
-        values |= {"_tag": node}
+        values |= {"_tag": elem}
     return moss.target(**values)
