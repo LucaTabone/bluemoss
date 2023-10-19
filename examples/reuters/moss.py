4a0b036ced7dd21d9230512ab1bcea45af0c1f87
lunchquader <luca.tabone93@gmail.com>
2023-10-19 16:30:16 +0200

    bugfix for example moss instance

--- examples/reuters/moss.py
+++ examples/reuters/moss.py
@@ -1,5 +1,5 @@
 from examples.reuters.classes import Article
-from bluemoss import Ex, Root, Range, extract
+from bluemoss import Ex, Root, Node, extract
 
 
 REUTERS_WORLD_NEWS_ARTICLES_MOSS: Root = Root(
@@ -7,16 +7,16 @@ REUTERS_WORLD_NEWS_ARTICLES_MOSS: Root = Root(
     path="li[contains(@class, 'story-collection')]/div[contains(@class, 'media-story-card')]",
     target=Article,
     nodes=[
-        Root(
+        Node(
             path="a",
             key="url",
             extract=Ex.HREF_ENDPOINT,
             transform=lambda endpoint: f"https://reuters.com{endpoint}"
         ),
-        Root(key="date", path="time"),
-        Root(key="title", path="h3/a"),
-        Root(key="img_url", path="img", extract="src"),
-        Root(key="topic", path="span/a", extract=Ex.TEXT)
+        Node(key="date", path="time"),
+        Node(key="title", path="h3/a"),
+        Node(key="img_url", path="img", extract="src"),
+        Node(key="topic", path="span/a", extract=Ex.TEXT)
     ]
 )
 
