1dc67eac1632a37a0c08ad3301c14adebf5bfcb4
lunchquader <luca.tabone93@gmail.com>
2023-10-20 17:56:34 +0200

    added test-file focused on testing path variations

---
+++ tests/test_path.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+import pytest
+from .constants import HIGH_NESTING_LEVEL_HTML
+from src.bluemoss import Root, Range, Ex, extract
+
+
+HTML: str = HIGH_NESTING_LEVEL_HTML
+
+
+def test_valid_paths():
+    Root()
+    Root("")
+    Root("a")
+    Root("//a")
+
+
+def test_invalid_path_1():
+    with pytest.raises(AssertionError):
+        Root("body///section")
+
+
+def test_invalid_path_2():
+    with pytest.raises(AssertionError):
+        Root("//")
+
+
+def test_invalid_path_3():
+    with pytest.raises(AssertionError):
+        Root("///a")
+
+
+def test_non_existent_path():
+    path: str = "h4"
+    assert extract(Root(path), HTML) is None
+    assert extract(Root(path, filter=3), HTML) is None
+    assert extract(Root(path, filter=None), HTML) == []
+
+
+def test_nested_path():
+    path: str = "body//section//li/a/../h3"
+    assert extract(Root(path, filter=None), HTML) == ["Service 1", "Service 2"]
+    assert extract(Root(path), HTML) == "Service 1"
+    assert extract(Root(path, filter=0), HTML) == "Service 1"
+    assert extract(Root(path, filter=1), HTML) == "Service 2"
+    assert extract(Root(path, filter=Range(0)), HTML) == ["Service 1", "Service 2"]
+    assert extract(Root(path, filter=Range(1)), HTML) == ["Service 2"]
+    assert extract(Root(path, filter=Range(4)), HTML) == []
+
+
+def test_nested_path_with_tag_attribute_search():
+    path: str = "body//section//li/a[@href='/info']/../h3"
+    assert extract(Root(path, filter=None), HTML) == ["Service 2"]
+    assert extract(Root(path), HTML) == "Service 2"
+
+
+def test_nested_path_with_text_search():
+    path: str = "html//section/ul//h3[text()='Service 1']/../a"
+    assert extract(Root(path), HTML) == "Learn More"
+    assert extract(Root(path, extract=Ex.HREF_ENDPOINT), HTML) == "/learn-more"
