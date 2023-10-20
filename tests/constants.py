3d07fb628f633c72dc67689b6989debe3a51c0bb
lunchquader <luca.tabone93@gmail.com>
2023-10-20 17:55:56 +0200

    exposed new html-test-file as constant

--- tests/constants.py
+++ tests/constants.py
@@ -9,3 +9,7 @@ with open(join(STATIC_DIR, "four_divs.html"), "r") as file:
 
 with open(join(STATIC_DIR, "with_links.html"), "r") as file:
     WITH_LINKS_HTML = file.read()
+
+
+with open(join(STATIC_DIR, "high_nesting_level.html"), "r") as file:
+    HIGH_NESTING_LEVEL_HTML = file.read()
