90612143f90a80ca8743c5ae705d421fb2013d24
lunchquader <luca.tabone93@gmail.com>
2023-10-22 21:39:02 +0200

    added placeholder funcs for test_invalid_moss.py

--- tests/test_invalid_moss.py
+++ tests/test_invalid_moss.py
@@ -1,2 +1,21 @@
 import pytest
-from src.bluemoss import Root, Node
+from src.bluemoss import (
+    Root, Node, InvalidXpathException,
+    InvalidTargetTypeException, InvalidKeysForTargetException, PartialKeysException
+)
+
+
+def test_invalid_xpath():
+    pass
+
+
+def test_invalid_target_type():
+    pass
+
+
+def test_invalid_keys_for_target():
+    pass
+
+
+def test_partial_keys_for_target():
+    pass
