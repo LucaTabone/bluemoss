103fc1135c7ed96782ccb55c2da4a9cbdfcdf506
lunchquader <luca.tabone93@gmail.com>
2023-10-24 18:56:00 +0200

    added package metadata as comments in src/bluemoss/__init__.py

--- src/bluemoss/__init__.py
+++ src/bluemoss/__init__.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'bluemoss'
+__author__ = 'LucaTabone'
+__license__ = 'APACHE'
+__copyright__ = 'Copyright 2023-2023 LucaTabone'
+__version__ = '0.0.1'
+
 from .classes import (
     BlueMoss, Node, Root, Range, Ex, InvalidTargetTypeException, InvalidKeysForTargetException,
     InvalidXpathException, PartialKeysException, EqualIndicesException, Jsonify, JsonifyWithTag
