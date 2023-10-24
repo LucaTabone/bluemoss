409ed2b870f19cd12e0fdb59361b5afaf48bcc4e
Luca Tabone <luca.tabone93@gmail.com>
2023-10-24 14:13:59 +0200

    updated noxfile to test for python versions 3.11 and 3.12

--- noxfile.py
+++ noxfile.py
@@ -1,17 +1,7 @@
-import sys
-import runpy
-from pathlib import Path
-
 import nox
 
-# Global configuration
-# https://nox.thea.codes/en/stable/config.html#modifying-nox-s-behavior-in-the-noxfile
-nox.options.error_on_missing_interpreters = False
 
-PIPELINES = Path(__file__).parent / 'pipelines'
-
-sys.path.append(str(Path.cwd()))
-
-for file in PIPELINES.iterdir():
-    if file.name.endswith('.nox.py'):
-        runpy.run_path(str(file))
+@nox.session(python=["3.11", "3.12"])
+def tests(session):
+    session.install("-r", "requirements.txt")
+    session.run("pytest")
