8dcb4070fdcfb1407d52c1d3b43689176e717612
lunchquader <luca.tabone93@gmail.com>
2023-10-25 05:57:33 +0200

    updated package version

--- src/bluemoss/__init__.py
+++ src/bluemoss/__init__.py
@@ -4,7 +4,7 @@ __title__ = 'bluemoss'
 __author__ = 'LucaTabone'
 __license__ = 'APACHE'
 __copyright__ = 'Copyright 2023-2023 LucaTabone'
-__version__ = '0.1.0'
+__version__ = '0.1.0-alpha.1'
 
 from .classes import (
     BlueMoss, Node, Root, Range, Ex, InvalidTargetTypeException, InvalidKeysForTargetException,
