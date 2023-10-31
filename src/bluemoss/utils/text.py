8efceb93bf353b14d241ea45acd581e8404b4d20
lunchquader <luca.tabone93@gmail.com>
2023-10-31 14:54:58 +0100

    added docs to /utils/text.py

--- src/bluemoss/utils/text.py
+++ src/bluemoss/utils/text.py
@@ -1,5 +1,20 @@
 def get_infix(text: str, prefix: str, suffix: str) -> str | None:
-    """ @return an infix within @param text given a specific prefix and a specific suffix. """
+    """
+    Compute the first string-infix in @param text that sits between @param prefix and @param suffix.
+    If either one of @param prefix and @param suffix is not found in the text, return None.
+    If both @param prefix and @param suffix are present in @param text, the returned string-infix would start
+    with the first character after @param prefix ends and ends with the first character before @param suffix starts.
+
+    Example 1: get_infix(text='Hello World!', prefix='ll', suffix='ld') returns 'o Wor'
+    Example 2: get_infix(text='Hello World!', prefix='Hey', suffix='World!') returns None
+    Example 3: get_infix(text='Hello World!', prefix='Hello', suffix='Darling') returns None
+
+    :param text: String to be searched
+    :param prefix: Prefix to be searched
+    :param suffix: Suffix to be searched
+    :rtype: str | None
+    :return: First string-infix in @param text that sits between @param prefix and @param suffix.
+    """
     if not text:
         return
     idx: int = text.find(prefix)
@@ -15,6 +30,18 @@ def get_infix(text: str, prefix: str, suffix: str) -> str | None:
 
 
 def clean_text(text: str | None) -> str | None:
+    """
+    Edit the given @param text by
+        - removing leading and trailing spaces
+        - replacing multiple spaces with just one space
+        - replacing any linebreaks + leading space with a linebreak
+        - replacing any linebreaks + trailing space with a linebreak
+        - replacing multiple newlines with just one newline
+
+    :param text: Text to be edited.
+    :rtype: str | None
+    :return: The updated text.
+    """
     if text is None:
         return
     text = text.strip()
