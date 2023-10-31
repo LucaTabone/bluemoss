fe5327e0bb4a85595653f6d8ade18f384d4d3ad9
lunchquader <luca.tabone93@gmail.com>
2023-10-31 14:55:25 +0100

    added docs to /utils/general.py

--- src/bluemoss/utils/general.py
+++ src/bluemoss/utils/general.py
@@ -5,17 +5,31 @@ from dataclasses import fields, Field, MISSING, is_dataclass
 
 @cache
 def get_required_class_init_params(cls) -> set[str]:
+    """
+    :param cls: class reference
+    :rtype: set[str]
+    :return: The set of required initialization parameters for the given class.
+    """
     return get_all_class_init_params(cls) - get_optional_class_init_params(cls)
 
 
 @cache
 def get_all_class_init_params(cls) -> set[str]:
-    assert inspect.isclass(cls)
+    """
+    :param cls: class reference
+    :rtype: set[str]
+    :return: The set of all initialization parameters for the given class.
+    """
     return set(inspect.getfullargspec(cls.__init__).args[1:])
 
 
 @cache
 def get_optional_class_init_params(cls) -> set[str]:
+    """
+    :param cls: class reference
+    :rtype: set[str]
+    :return: The set of optional initialization parameters for the given class.
+    """
     if is_dataclass(cls):
         return {
             f.name for f in fields(cls)
