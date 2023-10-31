64fc396c6b4df995a36712521e25e6cd67c81b50
lunchquader <luca.tabone93@gmail.com>
2023-10-31 14:55:09 +0100

    added docs to /utils/html.py

--- src/bluemoss/utils/html.py
+++ src/bluemoss/utils/html.py
@@ -3,7 +3,12 @@ from bs4 import BeautifulSoup
 from lxml.html import HtmlElement
 
 
-def is_valid_xpath(xpath_query: str):
+def is_valid_xpath(xpath_query: str) -> bool:
+    """
+    Checks if the given @param xpath_query is a valid XPath expression.
+    :rtype: bool
+    :return: True if @param xpath_query is a valid XPath expression, False otherwise.
+    """
     root = etree.Element("root")
     doc = etree.ElementTree(root)
     try:
@@ -14,21 +19,33 @@ def is_valid_xpath(xpath_query: str):
 
 
 def lxml_etree_to_bs4(tag: HtmlElement | str) -> BeautifulSoup | None:
+    """
+    Transforms a lxml.html.HtmlElement object to a BeautifulSoup object.
+    :rtype: BeautifulSoup | None
+    :return: BeautifulSoup representation of the given @param tag if isinstance(tag, HTMLElement), None otherwise.
+    """
     if isinstance(tag, HtmlElement):
         return BeautifulSoup(lxml_etree_to_string(tag), "html.parser")
 
 
 def lxml_etree_to_string(tag: HtmlElement | str) -> str | None:
+    """
+    Transforms a lxml.html.HtmlElement object to a string.
+    :rtype: str | None
+    :return: String representation of the given @param tag if isinstance(tag, HTMLElement), None otherwise.
+    """
     if isinstance(tag, HtmlElement):
         return etree.tostring(tag, method="html").decode("utf-8")
 
 
 def remove_tags_from_soup(soup: BeautifulSoup, tag_names: list[str]):
     """
-    Removes specific HTML tags from a BeautifulSoup object.
+    Removes html-tags with certain ids (@param tag_names) from the given BeautifulSoup object @param soup.
+    Exampl: if tag_names is ["h1", "h2"], then all "h1" and "h2" tags will be removed from @param soup.
 
     :param soup: BeautifulSoup object representing a block of HTML
     :param tag_names: Tag names to be removed HTML
+    :return: None
     """
     for data in soup(tag_names):
         data.decompose()
