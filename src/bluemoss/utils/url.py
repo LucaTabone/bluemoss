29e5247ad1399029091608b5360da5f486ef8c68
lunchquader <luca.tabone93@gmail.com>
2023-10-31 14:54:48 +0100

    added docs to url.py

--- src/bluemoss/utils/url.py
+++ src/bluemoss/utils/url.py
@@ -2,13 +2,33 @@ from urllib.parse import urlparse, parse_qs
 
 
 def get_base_domain(url: str | None) -> str | None:
-    """ @return: the top-level-domain of the given @param url. """
+    """
+    Returns the base domain of the given @param url.
+    Example:
+        1) get_base_domain("https://www.google.com?search=python") -> "google.com"
+        2) get_base_domain("https://www.products.apple.com?p=iphone") -> "apple.com"
+        3) get_base_domain("https://www.sub1.sub2.example.com/about/team?type=engineer") -> "example.com"
+        4) get_base_domain(None) -> None
+
+    :rtype: str | None
+    :return: The base-domain of the given @param url.
+    """
     domain: str | None = get_domain(url)
     return ".".join(domain.split(".")[-2:]) if domain else None
 
 
 def get_domain(url: str | None) -> str | None:
-    """ @return: the full domain of the given @param url. """
+    """
+    Returns the full domain of the given @param url.
+    Example:
+        1) get_domain("https://www.google.com?search=python") -> "google.com"
+        2) get_domain("https://www.products.apple.com?p=iphone") -> "products.apple.com"
+        3) get_domain("https://www.sub1.sub2.example.com/about/team?type=engineer") -> "sub1.sub2.example.com"
+        4) get_domain(None) -> None
+
+    :rtype: str | None
+    :return: The full domain of @param url.
+    """
     url: str | None = url.strip() if url else None
     if not url:
         return
@@ -17,7 +37,18 @@ def get_domain(url: str | None) -> str | None:
 
 
 def get_endpoint(url: str | None) -> str | None:
-    """ @return: the endpoint of the given @param url ."""
+    """
+    Returns the endpoint @param url.
+    Example:
+        1) get_endpoint("https://www.google.com?search=python") -> None
+        2) get_endpoint("https://www.products.apple.com?p=iphone") -> None
+        3) get_endpoint("https://www.sub1.sub2.example.com/about/team?type=engineer") -> "/about/team"
+        4) get_endpoint(None) -> None
+
+    :param url: The url to extract the base-domain from.
+    :rtype: str | None
+    :return: The endpoint of @param url.
+    """
     url: str | None = url.strip() if url else None
     if not url:
         return
@@ -29,6 +60,17 @@ def get_endpoint(url: str | None) -> str | None:
 
 
 def get_url_query(url: str | None) -> str | None:
+    """
+    Returns the query-string of @param url.
+    Example:
+        1) get_url_query("https://www.google.com?search=python") -> "search=python"
+        2) get_url_query("https://www.products.apple.com?p=iphone") -> "p=iphone"
+        3) get_url_query("https://www.sub1.sub2.example.com/about/team?type=engineer") -> "type=engineer"
+        4) get_url_query(None) -> None
+
+    :rtype: str | None
+    :return: The url-query-string of @param url.
+    """
     url: str | None = url.strip() if url else None
     if not url:
         return
@@ -37,14 +79,43 @@ def get_url_query(url: str | None) -> str | None:
 
 
 def get_endpoint_with_query(url: str | None) -> str | None:
+    """
+    Returns the endpoint including query-string of @param url.
+    Example:
+        1) get_endpoint_with_query("https://www.google.com?search=python") -> "?search=python"
+        2) get_endpoint_with_query("https://www.products.apple.com?p=iphone") -> "?p=iphone"
+        3) get_endpoint_with_query("https://www.sub1.sub2.example.com/about/team?type=engineer") 
+                -> "/about/team?type=engineer"
+        4) get_endpoint_with_query(None) -> None
+        5) get_endpoint_with_query(
+
+    :rtype: str | None
+    :return: endpoint + query of @param url.
+    """
     if not (endpoint := get_endpoint(url)):
-        return
+        endpoint = ""
     if not (query := get_url_query(url)):
+        query = ""
+    if not endpoint and not query:
+        return
+    if not query:
         return endpoint
     return f"{endpoint}?{query}"
 
 
 def get_url_query_params(url: str | None) -> dict:
+    """
+    Returns the query-parameters of @param url as a dict.
+    Example:
+        1) get_endpoint_with_query("https://www.google.com?search=python") -> {"search": "python"}
+        2) get_endpoint_with_query("https://www.products.apple.com?p=iphone") -> {"p": "iphone"}
+        3) get_endpoint_with_query("https://www.example.com/team?type=engineer&page=2&type=manager")
+                -> {"page": ["2"], "type": ["engineer", "manager"]}
+        4) get_endpoint_with_query(None) -> None
+
+    :rtype: dict
+    :return: The query-parameters of @param url.
+    """
     return parse_qs(urlparse(url).query) if url else {}
 
 
