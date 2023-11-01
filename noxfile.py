d6cbd2d24614984304694c0201f45afd3b812ea2
lunchquader <luca.tabone93@gmail.com>
2023-11-01 17:18:15 +0100

    update requirements.txt path in noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -3,5 +3,5 @@ import nox
 
 @nox.session(python=['3.11', '3.12'])
 def tests(session):
-    session.install('-r', 'requirements.txt')
+    session.install('-r', 'requirements/base.txt')
     session.run('pytest')
