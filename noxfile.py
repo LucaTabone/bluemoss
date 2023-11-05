ead4f9e0c03269e60bd2f45d91a2105556c2300e
Luca Tabone <luca.tabone93@gmail.com>
2023-11-05 12:48:33 +0100

    generate xml coverage report during nox session

--- noxfile.py
+++ noxfile.py
@@ -4,5 +4,6 @@ import nox
 @nox.session
 def tests(session):
     session.install('pytest')
-    session.install('-r', 'requirements/base.txt')
-    session.run('pytest')
+    session.install('-r', 'requirements/dev.txt')
+    session.run('coverage',  'run',  '-m', 'pytest')
+    session.run('coverage', 'xml', '-o', 'coverage.xml')
